57fb3be46294b6a3f053e3a17d68a31476ea1ef8
Bruno Guerra <gguerrabruno@outlook.com>
2022-07-03 22:24:34 -0300

    Changes to http-kit

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [ring/ring-core "1.9.5"]
-                 [ring/ring-jetty-adapter "1.9.5"]
+                 [http-kit "2.6.0"]
                  [metosin/reitit "0.5.18"]
                  [metosin/malli "0.8.9"]
                  [com.github.seancorfield/next.jdbc "1.2.780"]
--- src/clojure_crud/core.clj
+++ src/clojure_crud/core.clj
@@ -1,18 +1,20 @@
 (ns clojure-crud.core
   (:require [clojure-crud.routes :as routes]
             [muuntaja.core :as mu]
+            [org.httpkit.server :as h]
             [reitit.coercion.malli :as ma]
             [reitit.ring :refer [ring-handler router]]
             [reitit.ring.coercion :refer [coerce-request-middleware
                                           coerce-response-middleware]]
             [reitit.ring.middleware.muuntaja :refer [format-negotiate-middleware
                                                      format-request-middleware
-                                                     format-response-middleware]]
-            [ring.adapter.jetty :as j]))
+                                                     format-response-middleware]]))
 
 (def app-router
   (router
-    [[routes/food-routes]]
+    [["/api/v1"
+      [routes/food-routes]
+      ]]
     {:data {:coercion   ma/coercion
             :muuntaja   mu/instance
             :middleware [format-negotiate-middleware
@@ -24,5 +26,19 @@
 (def app
   (ring-handler app-router {:keywords? true :bigdecimals? true}))
 
+(defonce app-instance (atom nil))
+
+(defn start-server []
+  (reset! app-instance (h/run-server app {:port 3000 :join? false})))
+
+(defn stop-server []
+  (when @app-instance
+    (@app-instance :timeout 100))
+  (reset! app-instance nil))
+
+(defn restart-server []
+  (stop-server)
+  (start-server))
+
 (defn -main []
-  (j/run-jetty app {:port 3000}))
+  (h/run-server app {:port 3000}))
